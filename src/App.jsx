f1376c7fd8ba1c8b9c6abd84b26de5629c6c9f7f
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-30 01:32:13 +0600

    post data

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,19 @@ function App() {
     const form = event.target;
     const name = form.name.value;
     const email = form.email.value;
+    const user = { name, email};
+    console.log(user);
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log('inside post response',data)
+    })
   }
   return ( 
     <>
