915cdd2a9297ab63a43c49e0eeb01e86cd5686e9
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-05-30 00:25:19 +0600

    users management client

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([])
+
+
+  useEffect( () => {
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+  }
+  return ( 
+    <>
+      <h1>Users Management</h1>
+      <h3>Numbers of Users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.id} : {user.name} : {user.email}</p>)
+        }
+      </div>
+    </>
+  )
+}
+
+export default App;
